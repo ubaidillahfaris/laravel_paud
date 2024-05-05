b099a905ac29c5f4cb08afa3cf936e5c223dcc58
faris <ubaidillah.faris99@gmail.com>
2024-05-05 12:51:18 +0700

    feat: api auth, sanctum

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -49,10 +49,45 @@ class LoginController extends Controller
        }
     }
 
+    public function loginApi(Request $request){
+        try {
+            $request->validate([
+                'email' => 'required',
+                'password' => 'required'
+            ]);
+    
+            if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+    
+                $token = Auth::user()->createToken('auth_token')->plainTextToken;
+                // dd(Auth::user());
+                return response()->json([
+                    'message' => 'Login success',
+                    'access_token' => $token,
+                    'token_type' => 'Bearer'
+                ]);
+            }
+    
+           } catch (\Throwable $th) {
+            return response()
+            ->json([
+                'message' => 'gagal login'
+            ]);
+           }
+    }
+
     public function logout(Request $request){
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
         return redirect()->route('login');
     }
+
+    public function logoutApi()
+    {
+        Auth::user()->tokens()->delete();
+        return response()->json([
+            'message' => 'logout success'
+        ]);
+    }
 }
+
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+
+Route::post('/login', [LoginController::class, 'loginApi']);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [LoginController::class, 'logoutApi']);
+});
